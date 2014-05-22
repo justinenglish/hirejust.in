856f2bb76a9e0cb929344c5d4e021ff58f080eab
Justin English <justnenglish@gmail.com>
2014-05-22 12:22:47 -0700

    Added youtube API for Diablo 3 and World of Warcraft.

--- public/projects/anhsongaming/js/anhsongaming.js
+++ public/projects/anhsongaming/js/anhsongaming.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
     navButtons.removeClass('selected')
     $(this).addClass('selected')
   });
+  $.getJSON('https://www.googleapis.com/youtube/v3/search?part=id%2C+snippet&maxResults=10&key=AIzaSyCPBNiDrBlRIikP0OLD-uV9rVUl6WE2jH8&q=Blizzard%20Diablo%203', function(diablo) {
+    console.log(diablo);
+  });
+
+  $.getJSON('https://www.googleapis.com/youtube/v3/search?part=id%2C+snippet&maxResults=10&key=AIzaSyCPBNiDrBlRIikP0OLD-uV9rVUl6WE2jH8&q=World%20of%20Warcraft', function(wow) {
+    console.log(wow);
+  });
 });
