bd863a2e813624e555a247787b5830fa64f201c5
Justin English <justnenglish@gmail.com>
2014-05-23 18:26:40 -0700

    Fetch diablo 3 / wow videos from youtube, append to page

--- public/projects/anhsongaming/js/anhsongaming.js
+++ public/projects/anhsongaming/js/anhsongaming.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
+  window.videos = [];
   var navButtons = $('.nav ul li')
   navButtons.click(function() {
     navButtons.removeClass('selected')
     $(this).addClass('selected')
   });
   $.getJSON('https://www.googleapis.com/youtube/v3/search?part=id%2C+snippet&maxResults=10&key=AIzaSyCPBNiDrBlRIikP0OLD-uV9rVUl6WE2jH8&q=Blizzard%20Diablo%203', function(diablo) {
+    /*for(x = 0; x < diablo.items.length; x++) {
+      $('diablo').append($('boxes li'));
+    }*/
+    videos = videos.concat(diablo.items)
     console.log(diablo);
   });
 
   $.getJSON('https://www.googleapis.com/youtube/v3/search?part=id%2C+snippet&maxResults=10&key=AIzaSyCPBNiDrBlRIikP0OLD-uV9rVUl6WE2jH8&q=World%20of%20Warcraft', function(wow) {
+    /*for(x = 0; x < wow.items.length; x++) {
+      $('wow').append($('boxes li'));
+    }*/
+    videos = videos.concat(wow.items)
     console.log(wow);
   });
+
+
+  // Loop over the variable and output it to the grid (grey blocks)
+  window.outputToGrid = function(videos) {
+    for(x = 0; x < videos.length; x++) {
+      $('.boxes ul').append('<li style="background-image: url('+videos[x].snippet.thumbnails.default.url+')">' +videos[x].snippet.title+ '</li>')
+      console.log(videos[x].snippet.title)
+      //videos[]
+    }
+  }
+
 });
