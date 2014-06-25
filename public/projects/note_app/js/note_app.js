8e39c473c94bec88d24be11854e8eebdb0ba3160
Justin English <justnenglish@gmail.com>
2014-06-24 20:05:34 -0400

    Hitting ENTER in the text field will also add it to the list - note_app

--- public/projects/note_app/js/note_app.js
+++ public/projects/note_app/js/note_app.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
   $('#add_note').on('click', function(){
     var input = $("#text_input");
-    input.focus();
-    if(input.val() == "") {
-      return;
-    }
+      input.focus();
+      if(input.val() == "") {
+        return;
+      }
     $('ul').append("<li>"+input.val()+"</li>");
     input.val("");
   });
+  $("#text_input").keyup(function(e){
+    if(e.keyCode === 13) {
+    var input = $("#text_input");
+      input.focus();
+      if(input.val() == "") {
+        return;
+      }
+    $('ul').append("<li>"+input.val()+"</li>");
+    input.val("");
+    }
+  })
 });
