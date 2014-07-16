a5dfff7e4ad1f928dca39a112be8fb3725268595
Justin English <justnenglish@gmail.com>
2014-07-16 18:58:55 -0400

    Created js for project Calculator to make it functional. Also added hover colors for buttons.

--- public/projects/calculator/js/calculator.js
+++ public/projects/calculator/js/calculator.js
@@ -1,17 +1,36 @@
 $(document).ready(function() {
-  $('.buttons ul li').on('click', function(event) {
+  $('[data-buttons]').on('click', function(event) {
     //get input value
     var  input_value = $('.display').val();
     //get clicked value
     var button_value = $(event.currentTarget).attr('data-buttons');
+    //1. check if operator is already on display
+    //and at end of string.
+    var first_character = input_value.substr(0, 1);
+    var last_character = input_value.substr(input_value.length-1, 1);
+    var operators = ["/", "*", "-", "+"];
+    //2. last character is an operator
+    var is_input_operator = operators.indexOf(last_character) > -1;
+    var is_button_operator = operators.indexOf(button_value) > -1;
+    if(is_input_operator && is_button_operator) {
+      input_value = input_value.slice(0, input_value.length-1);
+    }
+    if(input_value == "" && button_value != "-" && is_button_operator) {
+      return;
+    }
+    // 3. pressing = should evaluate what the value is inside the input.
     //create combined value
     var new_value = input_value+button_value;
     //set input value to combined value
     $('.display').val(new_value);
   });
+  //create combined value
+  $('#equation').on('click', function() {
+    //set input value to combined value
+    var total = $('.display').val();
+    $('.display').val(eval(total));
+  });
   $('.clear').on('click', function() {
-    $('.display').reset();
-
-
+    $('.display').val('');
   });
 });
